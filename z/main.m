69132b14c6f42f9568ddc468fb6dccebe107ff87
katieekempp <40777203+katieekempp@users.noreply.github.com>
2021-02-16 04:02:07 -0500

    2/16/2021

--- z/main.m
+++ z/main.m
@@ -5,40 +5,38 @@ clear all, close all, clc
 % Transmitting coil
 I = 10; % filament current [A]
 turns = 10; % Numer of turn in the coil []
-radius = 10; % Radius of transmitting coil [mm?]
-tightness = 10000; % Increasing tightness will make the coil wrapped more tightly [1/mm?] THIS COULD BE CONSTANT?
-rho = 10; % resistivity of the wire
+radius = 10; % Radius of transmitting coil [m]
 
 % Receiving coil
 turns_car = 10; % Numer of turn in the coil []
-radius_car = 10; % Radius of transmitting coil [mm?]
-tightness_car = 10000; % Increasing tightness will make the coil wrapped more tightly [1/mm?] THIS COULD BE CONSTANT?
-rho_car = 10; % resistivity of the wire
+radius_car = 10; % Radius of transmitting coil [m]
 
 % Coil orientation
-height = 2; % Height of the receiving coil above the transmitting coil MUST BE ON THE GRID
-spacing = 30; % distance betewen centers of coils
+height = 2; % Height of the receiving coil above the transmitting coil [m]
+spacing = 30; % distance betewen centers of [m]
 velocity = 1; % velocity of car [m/s]
 
-%% FORMULA Constants
+%% Cost Analysis Variables
 
-distance = 160; % distance the car will travel [m]
-increment = .5; % Resolution (distance between the points) [m]
-efficiencyOfRectifier = 1; % BETWEEN 0 and 1. Approximates the efficiency of the full wave rectifier. We will assume it is 1.
-muRel = 1; % Relative permeability of road material + air combo [N/A^2?]
-scenarioID = 1; % scenario #
-timeStep = 1; % Distance between timesteps [s]
-dGamma = 1e9; % filament max discretization step [m]
+rho_car = 10; % resistivity of the wire [?] ?????
+L_car = 10000; % Length of the wire in the car [m]
+A_car = .001; % Cross sectional area of the wire in the car [m^2]
+rho = 10; % resistivity of the wire [?]
+V = 1; % voltage of power source [V]
+V_car = 12; % voltage of car battery [V]
+%gauge in road
+%gauge in car
 
 %% MAIN
 
-totalScenarios = 2;
-data = zeros(totalScenarios, 13);
+totalScenarios = 1;
+data = zeros(totalScenarios, 12);
 format shortG
 
 for i = 1:totalScenarios
-    totalCharge = DWPTz(I, turns, radius, tightness, rho, turns_car, radius_car, tightness_car, rho_car, height, spacing, velocity, distance, increment, efficiencyOfRectifier, muRel,scenarioID, timeStep, dGamma);
-    data(i,:) = [I turns radius tightness rho turns_car radius_car tightness_car rho_car height spacing velocity totalCharge];
+    scenarioID = i;
+    totalCharge = DWPTz(I, turns, radius, turns_car, radius_car, height, spacing, velocity, rho_car, L_car, A_car, scenarioID);
+    data(i,:) = [I turns radius turns_car radius_car height spacing velocity rho_car L_car A_car totalCharge];
 end
 
 writematrix(data,'data.csv')
