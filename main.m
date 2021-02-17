885d772491a0843ae742d43646b4c04a7bec634c
katieekempp <40777203+katieekempp@users.noreply.github.com>
2021-02-16 20:05:39 -0500

    2/16/2021

--- main.m
+++ main.m
@@ -3,11 +3,13 @@ clear all, close all, clc
 %% FORMULA variables
 
 % Transmitting coil
-I = 10; % filament current [A]
+V = 10; % Voltage of solar panels [V]
+wireGauge = 8; % Diameter of wire [m]
 turns = 10; % Numer of turn in the coil []
 radius = 10; % Radius of transmitting coil [m]
 
 % Receiving coil
+wireGauge_car = 12; % Diameter of wire [m]
 turns_car = 10; % Numer of turn in the coil []
 radius_car = 10; % Radius of transmitting coil [m]
 
@@ -18,15 +20,6 @@ velocity = 1; % velocity of car [m/s]
 
 %% Cost Analysis Variables
 
-rho_car = 10; % resistivity of the wire [ohm-m]
-L_car = 10000; % Length of the wire in the car [m]
-A_car = .001; % Cross sectional area of the wire in the car [m^2]
-V_car = 12; % voltage of car battery [V]
-%gauge in car?
-
-rho = 10; % resistivity of the wire [ohm-m]
-V = 1; % voltage of power source [V]
-%gauge in road?
 
 %% MAIN
 
@@ -36,8 +29,8 @@ format shortG % print data with the desired detail
 
 for i = 1:totalScenarios
     scenarioID = i;
-    totalCharge = DWPTeff(I, turns, radius, turns_car, radius_car, height, spacing, velocity, rho_car, L_car, A_car, scenarioID);
-    data(i,:) = [I turns radius turns_car radius_car height spacing velocity rho_car L_car A_car totalCharge];
+    totalCharge = DWPTeff(V, wireGauge, turns, radius, wireGauge_car, turns_car, radius_car, height, spacing, velocity, scenarioID);
+    data(i,:) = [V wireGauge turns radius wireGauge_car turns_car radius_car height spacing velocity totalCharge];
 end
 
 writematrix(data,'data/data.csv')
